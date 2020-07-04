73f52112f337c2a5dbe1441c02f53c265b2d190d
xjiangan <xjiangan@connect.ust.hk>
2020-07-04 21:49:43 +0800

    update for blender 2.8

--- render_mesh.py
+++ render_mesh.py
@@ -10,6 +10,10 @@ Each .blend file takes ~2.5MB set the save_blend_file flag to False if you don't
 Written by: Sagnik Das and Ke Ma
 Stony Brook University, New York
 December 2018
+
+Updated for blender 2.8 by Jiang Xudong
+HKUST
+July 2020
 '''
 
 
@@ -31,13 +35,13 @@ save_blend_file=True
 
 
 def reset_blend():
-    bpy.ops.wm.read_factory_settings()
-    bpy.context.scene.cursor_location = (0.0, 0.0, 0.0)
+    bpy.ops.wm.read_factory_settings()#...
+    bpy.context.scene.cursor.location = (0.0, 0.0, 0.0)#...
 
     # only worry about data in the startup scene
     for bpy_data_iter in (
             bpy.data.meshes,
-            bpy.data.lamps,
+            bpy.data.lights,
             bpy.data.images,
             bpy.data.materials
             ):
@@ -48,14 +52,14 @@ def reset_blend():
 def isVisible(mesh, cam):    
     bm = bmesh.new()   # create an empty BMesh
     bm.from_mesh(mesh.data) 
-    cam_direction = cam.matrix_world.to_quaternion() * Vector((0.0, 0.0, -1.0))
+    cam_direction = cam.matrix_world.to_quaternion() @ Vector((0.0, 0.0, -1.0))
     cam_pos = cam.location
     # print(cam_direction)
     mat_world = mesh.matrix_world
     ct1 = 0
     ct2 = 0
     for v in bm.verts:
-        co_ndc = world_to_camera_view(bpy.context.scene, cam, mat_world * v.co)
+        co_ndc = world_to_camera_view(bpy.context.scene, cam, mat_world @ v.co)
         nm_ndc = cam_direction.angle(v.normal)
         # v1 = v.co - cam_pos
         # nm_ndc = v1.angle(v.normal)
@@ -77,9 +81,9 @@ def isVisible(mesh, cam):
 
 def select_object(ob):
     bpy.ops.object.select_all(action='DESELECT')
-    bpy.context.scene.objects.active = None
-    ob.select=True
-    bpy.context.scene.objects.active = ob
+    bpy.context.view_layer.objects.active = None
+    ob.select_set(state=True)
+    bpy.context.view_layer.objects.active = ob
 
 
 def prepare_scene():
@@ -93,12 +97,12 @@ def prepare_scene():
     if random.random() > 0.5:
         bpy.data.scenes['Scene'].view_settings.view_transform='Filmic'
     else:
-        bpy.data.scenes['Scene'].view_settings.view_transform='Default'
+        bpy.data.scenes['Scene'].view_settings.view_transform='Standard'
 
 
 def prepare_rendersettings():
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.scenes['Scene'].cycles.device='CPU'
+    bpy.ops.object.select_all(action='DESELECT')#...
+    bpy.data.scenes['Scene'].cycles.device='GPU'#...
     bpy.data.scenes['Scene'].render.resolution_x=448
     bpy.data.scenes['Scene'].render.resolution_y=448
     bpy.data.scenes['Scene'].render.resolution_percentage=100
@@ -116,17 +120,19 @@ def add_lighting():
     wlinks=world.node_tree.links
     bg_node=wnodes['Background']
     # hdr lighting
-    # remove old node
+
+    remove old node
     for node in wnodes:
         if node.type in ['OUTPUT_WORLD', 'BACKGROUND']:
             continue
         else:
             wnodes.remove(node)
+
     # hdr world lighting
     if random.random() > 0.3:
         texcoord = wnodes.new(type='ShaderNodeTexCoord')
         mapping = wnodes.new(type='ShaderNodeMapping')
-        mapping.rotation[2] = random.uniform(0, 6.28)
+        mapping.inputs["Rotation"].default_value = (0.0, 0.0, random.uniform(0, 6.28)) 
         wlinks.new(texcoord.outputs[0], mapping.inputs[0])
         envnode=wnodes.new(type='ShaderNodeTexEnvironment')
         wlinks.new(mapping.outputs[0], envnode.inputs[0])
@@ -147,8 +153,8 @@ def add_lighting():
         eul.rotate_axis('X', random.uniform(math.radians(45), math.radians(135)))
         litpos.rotate(eul)
 
-        bpy.ops.object.add(type='LAMP', location=litpos)
-        lamp = bpy.data.lamps[0]
+        bpy.ops.object.add(type='LIGHT', location=litpos)
+        lamp = bpy.data.lights[0]
         lamp.use_nodes = True
         nodes=lamp.node_tree.nodes
         links=lamp.node_tree.links
@@ -167,7 +173,7 @@ def add_lighting():
 
     ## Area Lighting 
     # bpy.ops.object.lamp_add(type='AREA')
-    # lamp=bpy.data.objects[bpy.data.lamps[0].name]
+    # lamp=bpy.data.objects[bpy.data.lights[0].name]
     # select_object(lamp)
     # lamp.location=(0,0,10)
     # xt=random.uniform(-7.0,7.0)
@@ -177,7 +183,7 @@ def add_lighting():
     # bpy.ops.object.constraint_add(type='DAMPED_TRACK')
     # # bpy.data.objects[0].constraints['Damped Track'].target=bpy.data.objects['Empty']
     # lamp.constraints['Damped Track'].track_axis='TRACK_NEGATIVE_Z'
-    # lamp=bpy.data.lamps[bpy.data.lamps[0].name]
+    # lamp=bpy.data.lights[bpy.data.lights[0].name]
     # lamp.shape='RECTANGLE'
     # size_x=random.uniform(10,12)
     # size_y=random.uniform(1,3)
@@ -237,7 +243,7 @@ def reset_camera(mesh):
         eul.rotate_axis('Z', random.uniform(math.radians(-90 - st), math.radians(-90 + st)))
         
         camera.rotation_euler = eul
-        bpy.context.scene.update()
+        bpy.context.view_layer.update()
 
         if isVisible(mesh, camera):
             vid = True
@@ -246,14 +252,10 @@ def reset_camera(mesh):
         id += 1
     return vid
 
-def page_texturing(mesh, texpath):
-    select_object(mesh)
-    bpy.ops.object.mode_set(mode="OBJECT")
-    bpy.ops.object.material_slot_add()
-    bpy.data.materials.new('Material.001')
-    mesh.material_slots[0].material=bpy.data.materials['Material.001']
-    mat = bpy.data.materials['Material.001']
+def page_texturing(obj, texpath):
+    mat=bpy.data.materials.new('Material.001')
     mat.use_nodes = True
+    obj.material_slots[0].material=mat
     nodes = mat.node_tree.nodes
     # clear default nodes
     for n in nodes:
@@ -300,8 +302,8 @@ def page_texturing(mesh, texpath):
 
 def color_wc_material(obj,mat_name):
     # Remove lamp
-    for lamp in bpy.data.lamps:
-        bpy.data.lamps.remove(lamp, do_unlink=True)
+    for lamp in bpy.data.lights:
+        bpy.data.lights.remove(lamp, do_unlink=True)
 
     select_object(obj)
     # Add a new material
@@ -350,8 +352,8 @@ def get_worldcoord_img(img_name):
 
 def prepare_no_env_render():
     # Remove lamp
-    for lamp in bpy.data.lamps:
-        bpy.data.lamps.remove(lamp, do_unlink=True)
+    for lamp in bpy.data.lights:
+        bpy.data.lights.remove(lamp, do_unlink=True)
 
     world=bpy.data.worlds['World']
     world.use_nodes = True
@@ -363,17 +365,17 @@ def prepare_no_env_render():
     scene=bpy.data.scenes['Scene']
     scene.cycles.samples=1
     scene.cycles.use_square_samples=True
-    scene.view_settings.view_transform='Default'
+    scene.view_settings.view_transform='Standard'
 
 
 def render_pass(obj, objpath, texpath):
     # change output image name to obj file name + texture name + random three
     # characters (upper lower alphabet and digits)
-    fn = objpath.split('/')[-1][:-4] + '-' + texpath.split('/')[-1][:-4] + '-' + \
+    fn =os.path.split(objpath)[1][:-4] + '-' +os.path.split(texpath)[1][:-4] + '-' + \
         ''.join(random.sample(string.ascii_letters + string.digits, 3))
 
     scene=bpy.data.scenes['Scene']
-    scene.render.layers['RenderLayer'].use_pass_uv=True
+    #scene.render.layers['RenderLayer'].use_pass_uv=True
     bpy.context.scene.use_nodes = True
     tree = bpy.context.scene.node_tree
     links = tree.links
@@ -423,6 +425,8 @@ def render_img(objpath, texpath):
     prepare_scene()
     prepare_rendersettings()
     bpy.ops.import_scene.obj(filepath=objpath)
+    obj_object = bpy.context.selected_objects[0]
+    #print(obj_object)
     mesh_name=bpy.data.meshes[0].name
     mesh=position_object(mesh_name)
     add_lighting()
@@ -431,7 +435,7 @@ def render_img(objpath, texpath):
         return 1
     else:
         #add texture
-        page_texturing(mesh, texpath)
+        page_texturing(obj_object, texpath)
         fn = render_pass(mesh, objpath, texpath)
 
 
@@ -439,11 +443,11 @@ id1 = int(sys.argv[-2])
 id2 = int(sys.argv[-1])
 rridx = int(sys.argv[-3])
 
-path_to_output_images='./img/{}/'.format(rridx)
-path_to_output_uv = './uv/{}/'.format(rridx)
-path_to_output_wc = './wc/{}/'.format(rridx)
+path_to_output_images=os.path.abspath('./img/{}/'.format(rridx))
+path_to_output_uv = os.path.abspath('./uv/{}/'.format(rridx))
+path_to_output_wc = os.path.abspath('./wc/{}/'.format(rridx))
 if save_blend_file:
-    path_to_output_blends='./bld/{}/'.format(rridx)
+    path_to_output_blends=os.path.abspath('./bld/{}/'.format(rridx))
 
 for fd in [path_to_output_images, path_to_output_uv, path_to_output_wc, path_to_output_blends]:
     if not os.path.exists(fd):
@@ -463,9 +467,9 @@ with open(tex_list, 'r') as t, open(obj_list, 'r') as m:
     #print(objlist)
     for k in range(id1, id2):
         #print(k)
-        objpath = objlist[k][0]
+        objpath = os.path.abspath(objlist[k][0])
         idx = random.randint(0, len(texlist))
-        texpath=texlist[idx][0]
+        texpath=os.path.abspath(texlist[idx][0])
         print(objpath)
         print(texpath)
 
