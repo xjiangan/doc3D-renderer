f2e2920c92aea349c712b98dda14546d254a6132
xjiangan <xjiangan@connect.ust.hk>
2020-07-19 23:54:01 +0800

    configGUI

---
+++ configureGUI/window.cpp
@@ -0,0 +1,69 @@
+#include "window.h"
+
+Window::Window(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle(tr("Find Files"));
+    resize(1080, 360);
+    assetLayout = new QHBoxLayout;
+    loadButtonGroup = new QButtonGroup;
+    runButton = new QPushButton(tr("run"), this);
+    for (int i = 0; i < 4; i++)
+    {
+        assetLabel[i] = new QLabel(this);
+        assetListWidget[i] = new QListWidget(this);
+        assetVLayout[i] = new QVBoxLayout;
+        loadButton[i] = new QPushButton(tr("Load"), this);
+        loadButtonGroup->addButton(loadButton[i], i);
+        assetVLayout[i]->addWidget(assetLabel[i]);
+        assetVLayout[i]->addWidget(loadButton[i]);
+        assetVLayout[i]->addWidget(assetListWidget[i]);
+        assetLayout->addLayout(assetVLayout[i]);
+    }
+    assetLayout->addWidget(runButton);
+
+    assetLabel[0]->setText(tr("texture"));
+    assetLabel[1]->setText(tr("mesh"));
+    assetLabel[2]->setText(tr("env"));
+    assetLabel[3]->setText(tr("conf"));
+
+    setLayout(assetLayout);
+    connect(loadButtonGroup, QOverload<int>::of(&QButtonGroup::buttonClicked), this, &Window::loadAsset);
+    connect(runButton, &QPushButton::clicked, this, &Window::run);
+    //connect(texture, &QListWidget::currentTextChanged, texLable, &QLabel::setText);
+}
+void Window::run()
+{
+    QString program = "blender";
+    QStringList arguments{"--background", "--python", "render_mesh.py"};
+    QString paths[4];
+    for (int i = 0; i < 4; i++)
+    {
+        QListWidgetItem *item = assetListWidget[i]->currentItem();
+        if (item == nullptr)
+            return;
+        paths[i] = item->text();
+    }
+    arguments << "-t" << paths[0] << "-m" << paths[1] << "-e" << paths[2] << "-c" << paths[3];
+    QProcess *cmdProcess = new QProcess;
+    QObject::connect(cmdProcess, &QProcess::readyRead, [=]() {
+        QTextCodec *codec = QTextCodec::codecForName("GBK");
+        QString dir = codec->toUnicode(cmdProcess->readAll());
+        qDebug() << dir;
+    });
+    cmdProcess->start(program, arguments);
+}
+
+void Window::loadAsset(int id)
+{
+    QStringList directory = QFileDialog::getOpenFileNames(this, tr("Find Files"), QDir::currentPath());
+
+    for (int i = 0; i < directory.size(); i++)
+    {
+        new QListWidgetItem(QDir::current().relativeFilePath(directory[i]), assetListWidget[id]);
+    }
+}
+
+Window::~Window()
+{
+}
---
+++ configureGUI/window.h
@@ -0,0 +1,27 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+#include <QtWidgets>
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window(QWidget *parent = nullptr);
+    ~Window();
+
+    void loadAsset(int id);
+    void run();
+
+private:
+    QLabel *assetLabel[4];
+    QListWidget *assetListWidget[4];
+    QVBoxLayout *assetVLayout[4];
+    QPushButton *loadButton[4];
+    QHBoxLayout *assetLayout;
+    QButtonGroup *loadButtonGroup;
+    QPushButton *runButton;
+};
+#endif // WINDOW_H
