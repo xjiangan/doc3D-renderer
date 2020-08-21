852b4a3dda53aa3f2adf05f4d0cbf29765f8dfa3
xjiangan <xjiangan@connect.ust.hk>
2020-08-21 18:41:12 +0800

    check exist

--- uv2backwardmap/fm2bm.m
+++ uv2backwardmap/fm2bm.m
@@ -16,19 +16,19 @@ parfor k = 1 : n
         mkdir(t);
     end
     currf=files(k).name;
-    % if contains(currf,'.mat')
-    fname = fullfile(src_dir, currf);
-    disp(k);
-    d = load(fname);
-    % read uv
-    uv = d.uv;
-    uv = im2single(uv);
-    % flip v
-    uv = cat(3, uv(:, :, 3), 1.0 - uv(:, :, 2), uv(:, :, 1));
-    % compute backward mapping
-    bm = uv2mp(uv);
-    % save file (function save cannot be used with parfor, but the following can)
-    m=matfile(fullfile(t, currf), 'writable', true);
-    m.bm = bm;
-    % end
+    if ~exist(fullfile(t, currf),'file')
+        fname = fullfile(src_dir, currf);
+        disp(k);
+        d = load(fname);
+        % read uv
+        uv = d.uv;
+        uv = im2single(uv);
+        % flip v
+        uv = cat(3, uv(:, :, 3), 1.0 - uv(:, :, 2), uv(:, :, 1));
+        % compute backward mapping
+        bm = uv2mp(uv);
+        % save file (function save cannot be used with parfor, but the following can)
+        m=matfile(fullfile(t, currf), 'writable', true);
+        m.bm = bm;
+    end
 end
